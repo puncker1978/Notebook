048b4907b5579516978c1c2d2e6764b3c08a0fe6
Puncker1978 <nvv197827@gmail.com>
2021-11-30 20:42:35 +0500

    В класс Records добавил методы FindRecord для нахождения первого вхождения записи с подходящей фамилией и DeleteRecord для удаления первого вхождения сведений об одной записи с подходящей фамилией

--- Notebook/Records.cs
+++ Notebook/Records.cs
@@ -4,15 +4,28 @@ using System.Text;
 
 namespace Notebook
 {
+    /// <summary>
+    /// Статический класс для работы со списком записей
+    /// </summary>
     internal static class Records
     {
+        /// <summary>
+        /// Статический список записей
+        /// </summary>
         internal static List<Person> people = new List<Person>();
 
+        /// <summary>
+        /// Статический метод для добавления сведений об одной записи в список записей
+        /// </summary>
+        /// <param name="person">Сведения об одной записи</param>
         internal static void AddRecord(Person person)
         {
             people.Add(person);
         }
 
+        /// <summary>
+        /// Статический метод отображения всех записей, хранящихся в статическом списке
+        /// </summary>
         internal static void ShowRecords()
         {
             foreach (Person person in people)
@@ -28,6 +41,48 @@ namespace Notebook
                     $"Мобильный: {person.MobilePhone}\t" +
                     $"Рабочий: {person.WorkPhone}\t" +
                     $"Домашний: {person.HomePhone}");
+            }
+        }
+
+        /// <summary>
+        /// Метод поиска сведений об одной записи по Фамилии.
+        /// Находит первую запись с подходящей фамилией
+        /// </summary>
+        /// <param name="secondName">Фамилия для поиска</param>
+        /// <returns>Возвращает объект, сведения об одной записи</returns>
+        internal static Person FindRecord(string secondName)
+        {
+            Person person = null;
+
+            foreach (Person per in people)
+            {
+                if (per.SecondName == secondName)
+                {
+                    person = per;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+                return person;
+        }
+
+        /// <summary>
+        /// Статический метод удаления сведений об одной записи
+        /// при совпадении фамилии
+        /// </summary>
+        /// <param name="person">Сведения об одной записи</param>
+        internal static void DeleteRecord(Person person)
+        {
+            foreach(Person per in people)
+            {
+                if(person.SecondName == per.SecondName)
+                {
+                    people.Remove(per);
+                    break;
+                }
             }
         }
     }
